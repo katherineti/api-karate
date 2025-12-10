81f5b0903b121a10b0c613d1b3defe978092db48
katherineti <katherineti1609@gmail.com>
2025-12-09 20:05:14 -0400

    cor modificado

--- dist/src/main.js
+++ dist/src/main.js
@@ -11,8 +11,7 @@ async function bootstrap() {
             frontendLocalUrl,
             frontendUrl,
             'https://studio.firebase.google.com/',
-            'https://6000-firebase-studio-1764097833286.cluster-hlmk2l2htragyudeyf6f3tzsi6.cloudworkstations.dev',
-            'https://9000-firebase-studio-1764097833286.cluster-hlmk2l2htragyudeyf6f3tzsi6.cloudworkstations.dev',
+            'https://9000-firebase-studio-1764097833286.cluster-hlmk2l2htragyudeyf6f3tzsi6.cloudworkstations.dev/',
             '*'
         ],
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
